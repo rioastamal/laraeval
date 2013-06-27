8662e053c63ad4489ca8e32215e9211f977f60f7
Rio Astamal <me@rioastamal.net>
2013-06-27 07:19:52 +0700

    Update default string when user enter the code window for the first time

--- src/routes.php
+++ src/routes.php
@@ -8,26 +8,34 @@
 
 // Main page for entering the code
 Route::get('laraeval', function() {
-	$data = array(
-		'code' => '// enter your code here',
-		'output' => ''
-	);
-	return View::make('laraeval::form-code', $data);
+    $default_code = <<<CODE
+// Laraeval Shortcut
+// -----------------
+// CTRL+ENTER for executing the code
+// CTRL+, for switching to code window
+// CTRL+. for switching to output window
+CODE;
+
+    $data = array(
+        'code' => $default_code,
+        'output' => ''
+    );
+    return View::make('laraeval::form-code', $data);
 });
 
 // Let's eval the code
 Route::post('laraeval', array('as' => 'laraeval-main', function() {
-	$post = Input::get();
-	$laraeval = new Laraeval($post['code']);
-	$output = $laraeval->execute();
-	
-	if (strlen($output) === 0) {
-		$output = 'Code produces no output.';
-	}
-	
-	$data = array(
-		'code' => $post['code'],
-		'output' => $output
-	);
-	return View::make('laraeval::form-code', $data);
+    $post = Input::get();
+    $laraeval = new Laraeval($post['code']);
+    $output = $laraeval->execute();
+    
+    if (strlen($output) === 0) {
+        $output = 'Code produces no output.';
+    }
+    
+    $data = array(
+        'code' => $post['code'],
+        'output' => $output
+    );
+    return View::make('laraeval::form-code', $data);
 }));
